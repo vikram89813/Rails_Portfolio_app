ca9ec28b7ca16993bfe851d1b409fb8880a3f89e
Vikram Kumar <vikramkumar@Vikrams-MacBook-Pro.local>
2019-03-21 11:49:12 +0800

    Integrated Index action for Portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |blog|
+    Blog.create!(
+        title: "My blog post #{blog}",
+        body: "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled 
+        and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot 
+        foresee the pain and trouble that are bound to ensue; and equal blame belongs to 
+        those who fail in their duty through weakness of will, which is the same as saying through 
+        shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour,
+     when our power of choice is untrammelled and when nothing prevents our being able to do what we like 
+        best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and 
+        owing to the claims of duty or the obligations of business it will frequently occur that pleasures 
+        have to be repudiated and annoyances accepted. The wise man therefore always holds in these 
+        matters to this principle of selection: he rejects pleasures to secure other greater pleasures, 
+        or else he endures pains to avoid worse pains."
+    )
+end 
+
+puts "10 blog post created!"
+
+5.times do |skill|
+    Skill.create!(
+        title: "Rails #{skill}",
+        percent_utilized: 15
+    )
+end
+
+puts "5 skills created!"
+
+9.times do |portfolio|
+    Portfolio.create!(
+        title: "Portfolio title: #{portfolio}",
+        subtitle: "My Greate Service",
+        body: "matters to this principle of selection: he rejects pleasures to secure other",
+        main_image: "https://via.placeholder.com/650",
+        thumb_image: "https://via.placeholder.com/150"
+    )
+end
+
+puts "9 portfolio item created!"
