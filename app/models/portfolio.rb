1afa3eecf2f0394f8047d8d96be447fb6443ab99
Vikram Kumar <vikramkumar@Vikrams-MacBook-Pro.local>
2019-03-22 16:19:38 +0800

    Integrated nested attributes for portfolio item

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,6 +1,8 @@
 class Portfolio < ApplicationRecord
     validates_presence_of :title,:body, :main_image, :thumb_image
     has_many :technologies
+    accepts_nested_attributes_for :technologies,
+                                    reject_if: lamda {|attrs| attrs['name'].blank?} 
 
     def self.angular
         where(subtitle: 'Angular')
